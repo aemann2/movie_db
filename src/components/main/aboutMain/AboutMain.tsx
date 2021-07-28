e62980a52b098864d0f950467d66e3a6e2be1ff2
Adam Mann <aemann2@gmail.com>
2021-07-28 09:54:12 -0700

    linkedin optimization

--- src/components/main/aboutMain/AboutMain.tsx
+++ src/components/main/aboutMain/AboutMain.tsx
@@ -8,7 +8,7 @@ const AboutMain = () => {
 			<Picture />
 			<motion.div className='flex flex-col items-center max-w-xl px-10 py-10 mx-auto lg:text-2xl' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.2, duration: 0.5, ease: 'easeInOut' }}>
 				<p className='lg:text-3xl'>Hi, my name is Adam and I'm a librarian-turned-coder. I created this project to get some practice using Tailwind and Typescript.</p>
-				<h2 className='text-2xl lg:text-3xl text-center mt-10'>Main Technologies used:</h2>
+				<h2 className='text-2xl lg:text-3xl text-center mt-10'>Main Technologies Used:</h2>
 				<ul className='list-disc'>
 					<li>React</li>
 					<li>Tailwind</li>
