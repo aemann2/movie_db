6c85485213e772defd43b322e9d4ff49bafd54ca
Adam Mann <aemann2@gmail.com>
2021-07-29 16:52:33 -0700

    fixed search bar BG color

--- src/components/main/homeMain/components/searchBar/SearchBar.tsx
+++ src/components/main/homeMain/components/searchBar/SearchBar.tsx
@@ -25,7 +25,7 @@ const SearchBar = forwardRef<HTMLDivElement> ((props, ref) => {
 		<div ref={ref} className='text-center my-5 mx-auto flex justify-center max-w-5xl lg:my-10' id='search'>
 			<form onSubmit={handleSubmit} className='border border-mainText py-2 px-2 flex w-11/12 lg:w-9/12 xl:w-10/12 lg:border-2 rounded-xl lg:rounded-2xl'>
 				<SearchIconSVG className='h-5 sm:h-10 my-auto mr-2' />
-				<input className="text-sm sm:text-xl lg:text-3xl bg-background placeholder-headingText xs:w-8/12 sm:w-9/12 lg:py-3 pl-2" type='text' value={searchInput} placeholder="Search All Films..." onChange={(e) => setSearchInput(e.target.value)} />
+				<input className="text-sm sm:text-xl lg:text-3xl bg-transparent placeholder-headingText xs:w-8/12 sm:w-9/12 lg:py-3 pl-2" type='text' value={searchInput} placeholder="Search All Films..." onChange={(e) => setSearchInput(e.target.value)} />
 				<input className='border-0 text-xs sm:text-lg lg:text-2xl bg-attention ml-auto p-1 rounded-lg cursor-pointer' type='submit' value='Submit' />
 			</form>
 		</div>
